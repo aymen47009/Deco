9b8dca7f2e4dc9991ad0ed2ca561960d53272efb
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2026-07-26 21:50:03 +0100

    Deleted supabase.ts

--- src/components/FloatingBar.tsx
+++
@@ -1,9 +0,0 @@
-export function FloatingBar({ onClick }: { onClick: () => void }) {
-  return (
-    <button className="floating-bar" onClick={onClick} aria-label="اطلب الآن">
-      <span className="floating-bar-pulse" />
-      <span className="floating-bar-text">اطلب الآن</span>
-      <span className="floating-bar-arrow">←</span>
-    </button>
-  );
-}
