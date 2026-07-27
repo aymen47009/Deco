32e5efe6b97f70ee554d4ea1870324be14ea4bb6
aymen47009 <106097167+aymen47009@users.noreply.github.com>
2026-07-27 18:25:30 +0100

    Updated Project.js

--- server/src/models/Project.js
+++ server/src/models/Project.js
@@ -2,13 +2,6 @@ import mongoose from 'mongoose';
 
 const projectSchema = new mongoose.Schema(
   {
-      code: {
-    type: String,
-    unique: true,
-    default: () => `PRJ-${Date.now().toString(36).toUpperCase()}${Math.floor(Math.random() * 900 + 100)}`,
-  },
-  title: { type: String, required: true, trim: true },
-
     title: { type: String, required: true, trim: true },
     customer: { type: String, required: true, trim: true },
     phone: { type: String, required: true, trim: true },
